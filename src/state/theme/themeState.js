ad2380bdf6f6227a5756e01819fa07a0d82e2faa
David Hofer-Zeni <d.hoferzeni@saa.at>
2023-01-10 08:45:06 +0100

    Adding support for auto detecting client theme

--- src/state/theme/themeState.js
+++ src/state/theme/themeState.js
@@ -1,7 +1,13 @@
 import { useState } from "react";
 import themeContext from "../context/themeContext";
 const ThemeState = (props) => {
-  const [darkMode, setDarkMode] = useState(false);
+
+  // dark mode by default
+  let darkModeDefault = false;
+  if (window.matchMedia) {
+    darkModeDefault = !window.matchMedia('(prefers-color-scheme: dark)').matches;
+  }
+  const [darkMode, setDarkMode] = useState(darkModeDefault);
   return (
     <themeContext.Provider
       value={{
